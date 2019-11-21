f7a10b827a1c3641ca2af62a5080fbe020479ac2
yee2542 <yee2542gtl@gmail.com>
2019-11-22 01:16:59 +0700

    implement socket.io

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(8080, () => console.log('server running at 8080'))
+const server = http.createServer(app)
+server.listen(8080, () => console.log('server running at 8080'))
+const io = require('socket.io')(server)
+
+
+io.on('connection', socket => {
+    console.log('connection', socket.id)
+    
+    socket.on('screen', d => {
+        console.log('screen', socket.id, d)
+    })
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
+setInterval(() => {
+    // const list = io.
+    // console.log(list)
+}, 2000)
