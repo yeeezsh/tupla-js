3dcc9a7bcf730964c46143d8aafd53e7040eef99
yee2542 <yee2542gtl@gmail.com>
2019-11-23 01:29:27 +0700

    preparing render class

---
+++ render.random.js
@@ -0,0 +1,61 @@
+function ranIntRange(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+class Render {
+    constructor(w = 0, h = 0) {
+        this.maxWidth = w
+        this.maxHeight = h
+        this.lists = []
+    }
+
+    addObject() {
+        const object = {
+            x: ranIntRange(0, this.maxWidth),
+            y: ranIntRange(0, this.maxHeight),
+            color: getRandomColor()
+        }
+        this.lists.push(object)
+    }
+
+    removeObject() {
+        this.lists.pop()
+    }
+
+    update() {
+        const newFrame = this.lists.map(e => {
+            // console.log()
+            return {
+                ...e,
+                x: 0,
+                y: 0
+            }
+        })
+        this.lists = newFrame
+        return this.lists
+    }
+
+    showList() {
+        console.log(this.lists)
+    }
+
+
+}
+
+const test = new Render(100, 100)
+test.addObject()
+test.addObject()
+test.addObject()
+test.showList()
+
+
+module.exports = Render
