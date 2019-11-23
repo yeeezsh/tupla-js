fb920260e81ab8fb2a497077b5c5a7ec55a6d6a6
yee2542 <yee2542gtl@gmail.com>
2019-11-23 13:00:29 +0700

    fix heroku integration

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(process.cwd(), 'public')))
 
 const server = http.createServer(app)
 server.listen(8080, () => console.log('server running at 8080'))
